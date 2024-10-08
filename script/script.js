3d229f250f5a24f630eabfaf71e9a16771a1d79c
Usama-khanx <usamak2004@gmail.com>
2024-10-08 10:59:02 +0500

    connects bmi para and adds a division

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
 document.getElementById('calculate').addEventListener('click',function(){
-    var height = document.getElementById('height');
-    var weight = document.getElementById('weight');
+    var height = document.getElementById('height').value;
+    var weight = document.getElementById('weight').value;
+    var b = document.getElementById('bmi');
+    var bmi = weight/height;
+
+    b.textContent = bmi
+
+
+
 })
