f53475f4af1c4fa9f7ac099240d100fdea030f86
Usama-khanx <usamak2004@gmail.com>
2024-10-08 10:55:44 +0500

    add input fields and button and paragraph for a BMI and create a js file and connect input fields

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.getElementById('calculate').addEventListener('click',function(){
+    var height = document.getElementById('height');
+    var weight = document.getElementById('weight');
+})
